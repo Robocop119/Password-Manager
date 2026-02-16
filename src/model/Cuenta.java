6f3215fa87f55759aac9a43a0e7151c3feab1095
Mario <mariomartinezferreiro@gmail.com>
2026-02-16 12:22:04 +0100

    Añado getters y setters a Cuenta

---
+++ src/model/Cuenta.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Cuenta {
+    private String platform;
+    private String user;
+    private String password;
+
+    public String getplatform() {
+        return platform;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setplatform(String platform) {
+        this.platform = platform;
+    }
+
+    public void setUser(String user) {
+        this.user = this.user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
