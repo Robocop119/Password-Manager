0fd681291f2b44536e784b03e6fbb3e075081f66
Mario <mariomartinezferreiro@gmail.com>
2026-02-19 18:19:55 +0100

    Nueva Clase Gestor Claves

---
+++ src/logic/GestorClaves.java
@@ -0,0 +1,12 @@
+package logic;
+import model.Cuenta;
+import java.util.ArrayList;
+
+public class GestorClaves {
+    private ArrayList<Cuenta> listaCuentas;
+
+
+    public GestorClaves() {
+        this.listaCuentas = new ArrayList<>();
+    }
+}
--- src/model/Cuenta.java
+++ src/model/Cuenta.java
@@ -1,15 +1,19 @@
 package model;
 
 public class Cuenta {
+    // Declaramos las tres variables
     private String platform;
     private String user;
     private String password;
 
     public Cuenta(String platform, String user, String password) {
+        // Creamos el Constructor
         this.platform = platform;
         this.user = user;
         this.password = password;
     }
+
+    // Primero creamos los getters
 
     public String getplatform() {
         return platform;
@@ -23,12 +27,14 @@ public class Cuenta {
         return password;
     }
 
+    // Por último los setters
+
     public void setplatform(String platform) {
         this.platform = platform;
     }
 
     public void setUser(String user) {
-        this.user = this.user;
+        this.user = user;
     }
 
     public void setPassword(String password) {
